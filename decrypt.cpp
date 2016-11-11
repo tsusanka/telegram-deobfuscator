40f7ea0f0e538877482b9026622b3c8952f9d705
Tomas Susanka <tsusanka@gmail.com>
2016-11-11 10:26:57 +0100

    typos

--- decrypt.cpp
+++ decrypt.cpp
@@ -76,20 +76,20 @@ void decrypt(unsigned char *data, uint32_t length)
 	aesIgeEncryption(data + 24, key, key + 32, false, false, length - 24);
 
 	printf("ige decrypted:\n");
-    printHex(data + 24, length - 24);
+	printHex(data + 24, length - 24);
 
 	printf("\tsalt:\n");
-    printHex(data + 24, 8);
+	printHex(data + 24, 8);
 	printf("\tsession:\n");
-    printHex(data + 24 + 8, 8);
+	printHex(data + 24 + 8, 8);
 	printf("\tid:\n");
-    printHex(data + 24 + 16, 8);
+	printHex(data + 24 + 16, 8);
 	printf("\tmsg_seq:\n");
-    printHex(data + 24 + 24, 4);
+	printHex(data + 24 + 24, 4);
 	printf("\tmsg_size:\n");
-    printHex(data + 24 + 28, 4);
+	printHex(data + 24 + 28, 4);
 	printf("\tmsg:\n");
-    printHex(data + 24 + 32, length - 24 - 32);
+	printHex(data + 24 + 32, length - 24 - 32);
 	printAscii(data + 24 + 32, length - 24 - 32);
 
 	// last 8 are random id?
--- helpers.h
+++ helpers.h
@@ -1,7 +1,8 @@
 #ifndef HELPERS_H
 #define HELPERS_H
 
-void printHex(const void* buffer, size_t length);
+void printHex(const void *buffer, size_t length);
+
 void printAscii(unsigned char *data, size_t length);
 
 #endif
